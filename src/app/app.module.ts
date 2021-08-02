5b575e14a3cb85a5871a638714e368b13674f611
William Schneider <william.lima.schneider@gmail.com>
2021-08-02 11:31:58 -0300

    Product module, product service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,17 +9,30 @@ import { HeaderComponent } from "./components/template/header/header.component";
 import { MatToolbarModule } from "@angular/material/toolbar";
 import { FooterComponent } from "./components/template/footer/footer.component";
 import { NavComponent } from "./components/template/nav/nav.component";
-import { RedDirective } from './directives/red.directive';
+import { RedDirective } from "./directives/red.directive";
 
 import { MatSidenavModule } from "@angular/material/sidenav";
 import { MatListModule } from "@angular/material/list";
-import { HomeComponent } from './views/home/home.component';
-import { MatCardModule} from "@angular/material/card";
-import { ProductCrudComponent } from './views/product-crud/product-crud.component';
-import { ForDirective } from './directives/for.directive';
+import { HomeComponent } from "./views/home/home.component";
+import { MatCardModule } from "@angular/material/card";
+import { ProductCrudComponent } from "./views/product-crud/product-crud.component";
+import { ForDirective } from "./directives/for.directive";
+import { ProductCreateComponent } from "./components/product/product-create/product-create.component";
+import { MatButtonModule } from "@angular/material/button";
+import { MatSnackBarModule } from "@angular/material/snack-bar";
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, FooterComponent, NavComponent, HomeComponent, ProductCrudComponent, RedDirective, ForDirective],
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    NavComponent,
+    HomeComponent,
+    ProductCrudComponent,
+    RedDirective,
+    ForDirective,
+    ProductCreateComponent,
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
@@ -27,7 +40,9 @@ import { ForDirective } from './directives/for.directive';
     MatToolbarModule,
     MatSidenavModule,
     MatListModule,
-    MatCardModule
+    MatCardModule,
+    MatButtonModule,
+    MatSnackBarModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
